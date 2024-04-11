c29d13369f00dfd582b038291f033690b1d29920
Paul Faverjon <paul.faverjon@gmail.com>
2024-04-11 16:57:47 -0700

    .

--- All_models/run_multiple_scripts.py
+++ All_models/run_multiple_scripts.py
@@ -9,7 +9,7 @@ import subprocess
 def run_command():
     # Define the command to be executed
     command = """
-    srun -A ic_fintech296mb -p savio2 -t 00:30:00 -N 1 python run.py -model Autoformer --data Close --data_path close_data.csv --features S --dec_in 1 --enc_in 1 --target close_AAPL --freq h --use_gpu True --seq_len 24 --label_len 12 --pred_len 1 --e_layers 2 --d_layers 1 --factor 1 --d_model 32 --n_heads 4 --d_ff 512 --train_epochs 10
+    srun -A ic_fintech296mb -p savio2 -t 00:30:00 -N 1 python run.py -model Autoformer --data Close --data_path close_data.csv --features S --dec_in 1 --enc_in 1 --target close_AAPL --freq h --use_gpu True --seq_len 24 --label_len 12 --pred_len 1 --e_layers 2 --d_layers 1 --factor 1 --d_model 32 --n_heads 4 --d_ff 512 --train_epochs 1
     """
     
     # Execute the command
@@ -23,6 +23,6 @@ def run_command():
 if __name__ == "__main__":
     run_command()
 
-    
+
 
 
