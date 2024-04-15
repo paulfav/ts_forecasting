30e573908bf11dbd4ad8abb93dc9e34cf0fe950b
Paul Faverjon <95045179+paulfav@users.noreply.github.com>
2024-04-15 13:10:40 -0700

    change scripts

--- All_models/scripts.sh
+++ All_models/scripts.sh
@@ -1,18 +1,21 @@
-export CUDA_VISIBLE_DEVICES=1
-
-
 !/bin/bash
+
+#export CUDA_VISIBLE_DEVICES=1
+
 # Job name:
 SBATCH --job-name=test
 #
 # Account:
-SBATCH --account=paulfaverjon
+SBATCH --account=ic_fintech296mb
 #
 # Partition:
-SBATCH --partition=partition_name
+
+SBATCH --partition=savio2
 #
 # Wall clock limit:
-SBATCH --time=00:30:
+SBATCH --time=00:30:00
+
+SBATCH --nodes=1
 
 python -u run.py \
  --model Autoformer \
@@ -25,4 +28,14 @@ python -u run.py \
  --freq h \
  --use_gpu False
 
-python -u run.py --model Autoformer --data Close --data_path close_data.csv --features MS --dec_in 8 --enc_in 8 --c_out 8 --target close_AAPL --freq h --use_gpu False
+python -u run.py \
+    --model Autoformer \ 
+    --data Close \
+    --data_path close_data.csv \
+    --features MS \
+    --dec_in 8 \
+    --enc_in 8 \
+    --c_out 8 \
+    --target close_AAPL \
+    --freq h \
+    --use_gpu False
