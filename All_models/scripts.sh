85fb36a4c0e09a02163a1302f33af641affb2055
Paul Faverjon <95045179+paulfav@users.noreply.github.com>
2024-04-15 22:36:06 -0700

    modif script

--- All_models/scripts.sh
+++ All_models/scripts.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
-#export CUDA_VISIBLE_DEVICES=1
+# #export CUDA_VISIBLE_DEVICES=1
 
-# Job name:
-SBATCH --job-name=test
-#
-# Account:
-SBATCH --account=ic_fintech296mb
-#
-# Partition:
-SBATCH --partition=savio2
-#
-# Wall clock limit:
-SBATCH --time=00:30:00
+# # Job name:
+# SBATCH --job-name=test
+# #
+# # Account:
+# SBATCH --account=ic_fintech296mb
+# #
+# # Partition:
+# SBATCH --partition=savio2
+# #
+# # Wall clock limit:
+# SBATCH --time=00:30:00
 
-SBATCH --nodes=1
+# SBATCH --nodes=1
 
-echo "Starting run at: `date`"
+echo "Starting run at: `date`
 
-python -u run.py \
- --model Autoformer \
- --data Close \
- --data_path close_data.csv \
- --features S \
- --dec_in 1 \
- --enc_in 1 \
- --target close_AAPL \
- --freq h \
- --use_gpu False
+python -u run.py --model Autoformer --data Close --data_path close_data.csv --features S --dec_in 1 --enc_in 1 --target close_AAPL --freq h --use_gpu False
 
-python -u run.py \
-    --model Autoformer \ 
-    --data Close \
-    --data_path close_data.csv \
-    --features MS \
-    --dec_in 8 \
-    --enc_in 8 \
-    --c_out 8 \
-    --target close_AAPL \
-    --freq h \
-    --use_gpu False
+
+python -u run.py --model Autoformer --data Close --data_path close_data.csv --features MS --dec_in 8 --enc_in 8 --c_out 8 --target close_AAPL --freq h --use_gpu False
